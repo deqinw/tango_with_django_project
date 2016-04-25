1aac2763c91fe28a4efb05c14cd3ba2d8a250a88
deqinw <wangdeqin.aiesec@gmail.com>
2016-04-24 22:18:53 -0400

    Add comment to rango/urls.py

--- rango/urls.py
+++ rango/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import patterns, url
 from rango import views
 
 urlpatterns = [
-        url(r'^$', views.index, name='index'),
+        url(r'^$', views.index, name='index'), # name is actually optional
 ]
