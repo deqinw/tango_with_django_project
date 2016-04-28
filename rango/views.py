f15bab809e39e8d468322deeac2888f134726687
deqinw <wangdeqin.aiesec@gmail.com>
2016-04-27 21:46:14 -0400

    Add view and template for Category.

--- rango/views.py
+++ rango/views.py
@@ -1,4 +1,4 @@
-from rango.models import Category
+from rango.models import Category, Page
 from django.shortcuts import render
 
 def index(request):
@@ -12,6 +12,23 @@ def index(request):
     # Note that the first parameter is the template we wish to use.
     return render(request, 'rango/index.html', context_dict)
 
+
 def about(request):
     context_dict = {'about_message': 'Rango says here is the about page.'}
     return render(request, 'rango/about.html', context_dict)
+
+
+def category(request, category_name_slug):
+    context_dict = {}
+    try:
+        category = Category.objects.get(slug=category_name_slug) # If there isn't a category with given category_name_slug, a DoesNotExist exception will be thrown
+        context_dict['category_name'] = category.name
+        pages = Page.objects.filter(category=category)
+        context_dict['pages'] = pages
+        context_dict['category'] = category
+    except Category.DoesNotExist:
+        # We get here if we didn't find the specified category
+        # Don't do anything - the template displays the "no category" message.
+        pass
+
+    return render(request, 'rango/category.html', context_dict)
