d8333a0fa91176d8d422d1f281f1b0fb479932bb
deqinw <wangdeqin.aiesec@gmail.com>
2016-04-27 21:37:37 -0400

    Update admin with pre-populated slug field.

--- rango/admin.py
+++ rango/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from rango.models import Category, Page
 
-# Register your models here.
-admin.site.register(Category)
+# Add in this class to customize the Admin Interface
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('name',)}
+
+
+admin.site.register(Category, CategoryAdmin)
 admin.site.register(Page)
