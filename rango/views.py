78b105b77b3235c1f8dd03fb6bf90f357132d4b3
deqinw <wangdeqin.aiesec@gmail.com>
2016-04-27 23:12:14 -0400

    Update views with top 5 pages.

--- rango/views.py
+++ rango/views.py
@@ -5,7 +5,8 @@ def index(request):
     # Construct a dictionary to pass to the template engine as its context.
     # Note the key boldmessage is the same as {{ boldmessage }} in the template!
     category_list = Category.objects.order_by('-likes')[:5] # '-' means sort in descending order
-    context_dict = {'categories': category_list}
+    top_five_pages = Page.objects.order_by('-views')[:5]
+    context_dict = {'categories': category_list, 'pages':top_five_pages}
 
     # Return a rendered response to send to the client.
     # We make use of the shortcut function to make our lives easier.
