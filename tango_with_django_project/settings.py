cce1802cbf3842733a40dbe85a9b9460d9e2d7ab
deqinw <wangdeqin.aiesec@gmail.com>
2016-05-23 23:51:35 -0400

    Migrate to MySQL

--- tango_with_django_project/settings.py
+++ tango_with_django_project/settings.py
@@ -90,8 +90,11 @@ WSGI_APPLICATION = 'tango_with_django_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'tango_with_django_project',
+        'USER': 'tango_admin',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
     }
 }
 
